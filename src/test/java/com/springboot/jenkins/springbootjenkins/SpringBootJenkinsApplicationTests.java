ed6c0662fa738d34c8715e69f3420b44cfec58ae
Satish  Mulpuru <842842@TCS.com>
2019-02-06 12:34:39 +0530

    Spring Boot with Jenkins/Docker

---
+++ src/test/java/com/springboot/jenkins/springbootjenkins/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springboot.jenkins.springbootjenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
